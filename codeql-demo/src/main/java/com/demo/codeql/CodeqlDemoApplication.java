7630eb06f936bad60a22a2069a5d929a9eaf4b59
Miha Novak <m.miha.novak@gmail.com>
2022-06-08 10:47:51 +0200

    init commit

---
+++ codeql-demo/src/main/java/com/demo/codeql/CodeqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.codeql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeqlDemoApplication.class, args);
+	}
+
+}
